c602a4f8f1a72e35c07ad1215ac88917a0424950
Gleb Vorobjev <glebvorobjev@gmail.com>
2014-12-06 13:20:22 -0500

    Fix script tags, add color button event listeners

--- coatOfArmsApp.js
+++ coatOfArmsApp.js
@@ -1,1 +1,40 @@
+//Cache the shield selector
+var $shield = $("#shield");
+
+//Declare the color values
+var orColor = "#FFFF22";
+var argentColor = "#FFFFFF";
+var azureColor = "#0033CC";
+var gulesColor = "#FF0000";
+var purpureColor = "#CC3399";
+var sableColor = "#000000";
+var vertColor = "#009900";
+
 //Button color controls
+$("#or").click( function() {
+  $shield.css("background-color", orColor);
+});
+
+$("#argent").click( function() {
+  $shield.css("background-color", argentColor);
+});
+
+$("#azure").click( function() {
+  $shield.css("background-color", azureColor);
+});
+
+$("#gules").click( function() {
+  $shield.css("background-color", gulesColor);
+});
+
+$("#purpure").click( function() {
+  $shield.css("background-color", purpureColor);
+});
+
+$("#sable").click( function() {
+  $shield.css("background-color", sableColor);
+});
+
+$("#vert").click( function() {
+  $shield.css("background-color", vertColor);
+});
