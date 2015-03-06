caba40013d558c15c31d976cee3f2afcc5d87d5c
Gleb Vorobjev <glebvorobjev@gmail.com>
2015-03-05 22:15:42 -0500

    Add list of buttons bound to field color

--- main.js
+++ main.js
@@ -2,11 +2,13 @@
   var app = angular.module("main", []);
 
   app.controller("shieldController", function(){
-    this.fieldColor = "orColor";
+    this.fieldColor = "defaultColor";
     this.ordinary =  {
       ordinaryType: "default",
       ordinaryColor: "defaultColor"
     };
+
+    this.COLORS = ["orColor", "argentColor", "azureColor", "gulesColor", "purpureColor", "sableColor", "vertColor"];
   });
 
   
